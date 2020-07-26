9737ab1b7a9684c500a828c77d3dae2c6d64c1f0
natipoi <yota@natipoi.com>
2020-07-27 00:42:54 +0900

    ブログ機能

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,6 @@
 
 require("dotenv").config();
+const config = require('./.contentful.json')
 
 export default {
   /*
@@ -81,7 +82,11 @@ export default {
     vendor: ["aframe"]
   },
   env: {
-    baseUrl: process.env.BASE_URL || "https://fitpoi.com"
+    baseUrl: process.env.BASE_URL || "https://fitpoi.com",
+    CTF_SPACE_ID: config.CTF_SPACE_ID,
+    CTF_CDA_ACCESS_TOKEN: config.CTF_CDA_ACCESS_TOKEN,
+    CTF_PERSON_ID: config.CTF_PERSON_ID,
+    CTF_BLOG_POST_TYPE_ID: config.CTF_BLOG_POST_TYPE_ID
   },
   router: {
     base: "/",
---
+++ pages/posts/_id/index.vue
@@ -0,0 +1,52 @@
+<template>
+  <section class="container">
+    <div class="h1-box">
+    <h1>{{ post.fields.title }}</h1>
+    <p class="createdat">{{ post.fields.publishDate }}</p>
+    </div>
+    <div class="hero-image">
+    <img :src="post.fields.heroImage.fields.file.url" :alt="post.fields.heroImage.fields.description">
+    </div>
+    <div v-html="mkbody" class="main-body" />
+  </section>
+</template>
+
+<style>
+.h1-box {margin: 0 30px;}
+h1 {margin-bottom:0;}
+.hero-image img {width: 100%;}
+.main-body {margin: 30px;}
+.createdat {color: #969594; font-size: 1.0rem; text-align: right;margin-bottom: 5px;}
+</style>
+
+<script>
+import { createClient } from '~/plugins/contentful.js'
+import marked from 'marked';
+import hljs from 'highlight.js';
+
+const client = createClient()
+export default {
+  async asyncData({ params, payload }) {
+    // IDをキーに記事を取得
+    const entry = await client.getEntry(params.id)
+
+    console.log(entry)
+    return {
+      post: entry
+    }
+  },
+  computed: {
+    mkbody() {
+      return marked(this.post.fields.body);
+    }
+  },
+  created() {
+    marked.setOptions({
+      langPrefix: '',
+      highlight: function(code, lang) {
+        return hljs.highlightAuto(code, [lang]).value
+      }
+    })
+  }
+}
+</script>
